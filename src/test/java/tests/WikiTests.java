4ebea56502450ac1c246806be29aa8c8fc8fdcec
John Smoker <testgits@yandex.ru>
2021-06-01 19:00:29 +0500

    Added steps

--- src/test/java/tests/WikiTests.java
+++ src/test/java/tests/WikiTests.java
@@ -1,21 +1,38 @@
 package tests;
 
 import io.appium.java_client.MobileBy;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
 import static com.codeborne.selenide.Selenide.*;
 import static io.appium.java_client.MobileBy.AccessibilityId;
+import static io.qameta.allure.Allure.step;
 
 public class WikiTests extends TestBase {
     @Test
-    void simpleWikiTest() {
-        open();
-        if ($(MobileBy.id("org.wikipedia.alpha:id/view_onboarding_page_indicator")).isDisplayed()) {
-            back();
-        }
-        $(AccessibilityId("Search Wikipedia")).click();
-        $(MobileBy.id("org.wikipedia.alpha:id/search_src_text")).setValue("Browserstack");
-        $$(MobileBy.id("org.wikipedia.alpha:id/search_results_list")).shouldHave(sizeGreaterThan(0));
+    @DisplayName("Simple search test")
+    void simpleWikiSearchTest() {
+        step("Open app", () -> {
+            open();
+        });
+
+        step("If opened onboarding page - press back button", () -> {
+            if ($(MobileBy.id("org.wikipedia.alpha:id/view_onboarding_page_indicator")).isDisplayed()) {
+                back();
+            }
+        });
+
+        step("Click on 'Search Wikipedia'", () -> {
+            $(AccessibilityId("Search Wikipedia")).click();
+        });
+
+        step("Type 'Browserstack'", () -> {
+            $(MobileBy.id("org.wikipedia.alpha:id/search_src_text")).setValue("Browserstack");
+        });
+
+        step("Verify success search", () -> {
+            $$(MobileBy.id("org.wikipedia.alpha:id/search_results_list")).shouldHave(sizeGreaterThan(0));
+        });
     }
 }
